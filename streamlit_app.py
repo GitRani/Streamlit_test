4d5cdadc93ed289786bdb838b375fcd951d2ee00
GitRani <55916123+GitRani@users.noreply.github.com>
2024-06-11 22:13:17 +0900

    Update streamlit_app.py
    progress bar test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
 # streamlit template test
 
 import streamlit as st
+import time
 
+progress_bar = st.progress(0)
+
+for i in range(100):
+    progress_bar.progress(i + 1)
+    time.sleep(0.1)
 
 # st.title('Streamlit text')
 
